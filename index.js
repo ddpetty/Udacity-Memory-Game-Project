0c3ecb35f38c9477cae753c48c175031e59a548c
ddpetty <dionne.petty@gmail.com>
2018-05-12 14:14:50 -0400

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
 window.onload = () => {
 
-	let cards = document.querySelectorAll('.card'),
-		selectedCards = [],
-		cardFlipped = false,
-		numCardsFlipped = 0,
-		cardBacks = document.querySelectorAll('.back'),
-		symbols = ['<i class="fas fa-coffee"></i>', '<i class="fas fa-code-branch"></i> ', '<i class="fas fa-code"></i>', '<i class="fas fa-window-close"></i>', '<i class="fas fa-file-code"></i>', '<i class="fas fa-terminal"></i>', '<i class="fas fa-qrcode"></i>', '<i class="fas fa-user-secret"></i>'];
+	let cardBacks = document.getElementsByClassName('.back'),
+	cards = document.querySelectorAll('.card'),
+	cardFlipped,
+		cardImages = ['<i class="fas fa-coffee"></i>', '<i class="fas fa-coffee"></i>', '<i class="fas fa-code-branch"></i>', '<i class="fas fa-code-branch"></i>', '<i class="fas fa-code"></i>', '<i class="fas fa-code"></i>', '<i class="fas fa-window-close"></i>', '<i class="fas fa-window-close"></i>', '<i class="fas fa-file-code"></i>', '<i class="fas fa-file-code"></i>', '<i class="fas fa-terminal"></i>', '<i class="fas fa-terminal"></i>', '<i class="fas fa-qrcode"></i>', '<i class="fas fa-qrcode"></i>', '<i class="fas fa-user-secret"></i>', '<i class="fas fa-user-secret"></i>'],
+		resetButton = document.querySelector('.reset'),
+		numCardsFlipped = 0;
 
+	// Shuffle function is from http://stackoverflow.com/a/2450976
+	let shuffleCards = (array) => {
+		let currentIndex = array.length,
+			temporaryValue, randomIndex;
 
-	// Array has two of each, then randomize it
-	let possibleSelectedCards = symbols.slice(0);
-	for (let i = 0; i < 8; i++) {
-		// Randomly pick one from the array of symbols left
-		let randomIndex = Math.floor(Math.random(possibleSelectedCards.length)),
-			symbol = possibleSelectedCards[randomIndex];
-		// Push two copies or symbols onto selectedCards array
-		selectedCards.push(symbol);
-		selectedCards.push(symbol);
-		// Remove from array
-		possibleSelectedCards.splice(randomIndex, 1);
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+		return array;
+	}
+	shuffleCards(cardImages);
+
+	//Loop over cardImages and output to back of cards
+	cardImages.forEach((cardImage) => {
+    cardBacks.innerHTML = cardImage;
+	console.log(cardBacks.innerHTML);
+
+	});
+
+	let resetGame = () => {
+		let resetBtn = document.querySelector('.reset');
+		resetButton.addEventListener('click', newGame());
 	}
 
-	//Randomize the selectedCards array
-	selectedCards.sort(() => {
-		return 0.5 - Math.random();
-	});
-	console.log(selectedCards);
+	let newGame = () => {
+		numCardsFlipped = 0;
+		let cardOutput = '';
 
-	//Create divs that hold symbols & push to back of card
-	let randomizedSymbols = [];
-	let symbolSpans = document.createElement('span');
-	symbolSpans.innerHTML = selectedCards.pop();
-	randomizedSymbols.push(symbolSpans.innerHTML);
-
+	}
 	// Checks if card doesn't have class, adds is-flipped class, then flips it back over after 4 sec.
 	let flipCard = () => {
 		cards.forEach((card) => {
 			if (!card.classList.contains('is-flipped')) {
-				card.addEventListener('click', () => {
+				card.addEventListener('click', function (e) {
 					card.classList.add('is-flipped');
 					cardFlipped = true;
 					// setTimeout(() => {
@@ -49,15 +67,5 @@ window.onload = () => {
 		});
 	}
 	flipCard();
-
-	// let startGame = () => {
-	// 	reset cards
-	// 	Shuffle Cards
-	// 	add symbols to back of cards
-	// 	flip two cards
-	// 	if they match, both stay open
-	// 	if they don't, they flip back over
-	// 	keep doing this until all cards are flipped
-	// 	game over display message (The game is over! Press the reset button for a new game.)
 
 }
